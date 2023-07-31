446c2a7627c999278b69094c45718128a749e47d
Kona <landon.deroin@gmail.com>
2023-07-31 19:01:49 -0400

    added middlewear and port to server

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const path = require('path');
 const fs = require('fs');
 // making express a varible
 const app = express();
+// defines which port to use either with heroku or local
+const PORT = process.env || 3001;
+// middlewear
+app.use(express.static('public'));
