a07c1f0eac911135c73d33f1dd85d70c529d5295
Kona <landon.deroin@gmail.com>
2023-07-31 18:51:46 -0400

    initial file structure and some varibles in server js

---
+++ server.js
@@ -0,0 +1,6 @@
+// importing the paths, fs, and express
+const express = require(express);
+const path = require('path');
+const fs = require('fs');
+// making express a varible
+const app = express();
